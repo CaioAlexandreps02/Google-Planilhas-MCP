9cae81a895501a79b9a38236edf8b13026c9b7da
Caio Embrepoli <caio@embrepoli.com.br>
2026-08-07 09:19:15 -0300

    Adiciona find_bound_script para descobrir o ID do script sem abrir o editor
    Usa a Drive API (escopo drive.metadata.readonly) para achar o projeto de
    Apps Script vinculado a uma planilha, listando os filhos com mimeType
    application/vnd.google-apps.script.

--- lib/google-oauth.ts
+++ lib/google-oauth.ts
@@ -3,6 +3,7 @@ import { google } from "googleapis";
 export const OAUTH_SCOPES = [
   "https://www.googleapis.com/auth/spreadsheets",
   "https://www.googleapis.com/auth/script.projects",
+  "https://www.googleapis.com/auth/drive.metadata.readonly",
 ];
 
 export function getOAuthClient(redirectUri: string) {
